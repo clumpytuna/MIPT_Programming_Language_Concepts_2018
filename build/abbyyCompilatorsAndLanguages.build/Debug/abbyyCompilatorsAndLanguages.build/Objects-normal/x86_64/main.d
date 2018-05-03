8094147f1764982d34cd0f7e903d7c2f1566a374
clumpyTuna <anuhinm@mail.ru>
2018-05-03 16:15:50 +0300

    Delete main.d

--- build/abbyyCompilatorsAndLanguages.build/Debug/abbyyCompilatorsAndLanguages.build/Objects-normal/x86_64/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/main.cpp \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/unwindingObj.hpp \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/scopeBuffer.hpp \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/global.h \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/testObject.hpp
