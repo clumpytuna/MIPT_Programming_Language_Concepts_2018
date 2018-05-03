cec1599e1a6e596aa186c5e7458c62a39fac68b7
clumpyTuna <anuhinm@mail.ru>
2018-05-03 16:16:32 +0300

    Delete testObject.d

--- build/abbyyCompilatorsAndLanguages.build/Debug/abbyyCompilatorsAndLanguages.build/Objects-normal/x86_64/testObject.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/testObject.cpp \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/testObject.hpp \
-  /Users/clumpytuna/Documents/My\ Documents/abbyyCompilatorsAndLanguages/abbyyCompilatorsAndLanguages/unwindingObj.hpp
